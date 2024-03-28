409fe36b5b22a071d837de36249cb9f2833ec8ca
tomacoo13 <juank133@hotmail.es>
2024-03-28 19:54:07 +0100

    28-03

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
-import App from './dolares.jsx'
+// import App from './dolares.jsx'
+import App from './ej-usestate-useref/euros-dolares.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
