128735c595324723ecaa1c0f9c17ba6591eedd0d
Juan Carlos <juank133@hotmail.es>
2024-04-05 13:08:11 +0200

    05/04(casa)

--- src/components/Componenteconestilo.jsx
+++ src/components/Componenteconestilo.jsx
@@ -1,4 +1,12 @@
-const Popup = ({ visible }) => <Back visible={visible} />;
+import { Back } from "./styleofcomponents"
 
+const Popup = ({ visible, setVisible }) => {
+    <Back visible = {visible}> 
+    <div>
+        <p>hola</p>
+    <button onClick={ () => setVisible(!setVisible)}>Close</button>
+    </div>
+    </Back>
+}
 
 export default Popup;
--- src/components/styleofcomponents.js
+++ src/components/styleofcomponents.js
@@ -1,32 +1,43 @@
-import styled from "styled-components";;
+import styled from "styled-components";
 
-export const Box = styled.div`   //Puede ser cualquier etiqueta (.button, .p, .div...)
-width: 100px;
-height: 100px;
-background-color: red;
+// export const Box = styled.div`   //Puede ser cualquier etiqueta (.button, .p, .div...)
+// width: 100px;
+// height: 100px;
+// background-color: red;
 
-/* background-color:${(props) => props.color}; */
-/* margin:${(prop) => prop.m}px; */
-/* background-color:red; */
+// /* background-color:${(props) => props.color}; */
+// /* margin:${(prop) => prop.m}px; */
+// /* background-color:red; */
 
-background-color:${({ color }) => color || "red"};
-margin-bottom: ${({ m }) => m}px;
+// background-color:${({ color }) => color || "red"};
+// margin-bottom: ${({ m }) => m}px;
 
-span {
-    font-weight: bold;
-}
+// span {
+//     font-weight: bold;
+// }
 
-`;
+// `;
+// export const MyStyledComponent = styled.div`
+// width: 100px;
+// height: 100px;
+// background-color:${({ color }) => color || 'yellow'} ;
+// `;
 
-export const MyStyledComponent = styled.div`
-width: 100px;
-height: 100px;
 
-background-color:${({ color }) => color || 'yellow'} ;
+export const Back = styled.div`
+    background-color:blue;
+    width: 100vw;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    display: ${ ({visible}) => visible? 'block': 'none'};
+    position:fixed;
+    top:0;
+    left:0;
 
-`;
-
-export const Popup = styled.div`
-
-display:none:${({!visible}) => visible || display: auto}
-`;
+    div{
+        background-color: white;
+    width: 600px;
+    height:600px;
+    }
+`
