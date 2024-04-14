dfefc47bb3eb256d4aeac0591b97278ea4a46ffb
Juan Carlos <juank133@hotmail.es>
2024-04-14 14:10:09 +0200

    14/04 casa workoutapp

--- src/components/Workout/Workout.styles.js
+++ src/components/Workout/Workout.styles.js
@@ -14,20 +14,28 @@ export const ColRight = styled.div`
  display: flex;
  width: 700px;
  flex-wrap: wrap;
+ background-color: SlateGray;
 
  img {
   width: 300px;
   height: 300px;
-  border: 1px solid black;  
   border-radius: 20px;
   margin: 10px;
+  cursor:pointer;
  }
 `;
 
 export const ColLeft = styled.div`
  width: 700px;
- background-color:grey;
- border-radius: 20px;
+ background-color:SlateGray;
+ display: flex;
+
+ img {
+  width:620px;
+  height:620px ;
+  border-radius: 20px;
+  margin: auto;
+ }
  `;
 
  export const Menu = styled.div`
@@ -41,13 +49,18 @@ background-repeat: no-repeat;
 margin-top: 10px;
 width: 60px; 
 height: 60px; 
+cursor:pointer;
 `
-export const BtnLeft = styled.img`
+export const BtnLeft = styled.span`
 ${Btn}
 margin-right: 10px; 
 `
-export const BtnRight = styled.img`
+export const BtnRight = styled.span`
 ${Btn}; 
 margin-left: 10px; 
-transform: scaleX(-1)
+transform: scaleX(-1);
+`
+export const Img = styled.img`
+border-radius: 20px;
+    ${({ active }) => active ? 'border: 4px solid red; z-index:1' : 'border:4px solid transparent'}
 `
